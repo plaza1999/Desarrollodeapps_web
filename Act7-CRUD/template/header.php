ddb49b8e4fa50edf87cf089a641beef983b9c01c
plaza1999 <135294818+plaza1999@users.noreply.github.com>
2023-07-16 23:42:23 -0500

    Add files via upload

---
+++ Act7-CRUD/template/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <title>Actividad #7 CRUD PHP</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<!-- cdn icnonos-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+</head>
+<body>
+    <div class="container-fluid bg-warning">
+    
+        <div class="row">
+            <div class="col-md">
+                <header class="py-3">
+                    <h3 class="text-center">Actividad #7 CRUD PHP</h3>
+                </header>
+            </div>
+        </div>
+    </div>
